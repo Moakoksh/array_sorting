2821251386674bbdcbe791573933a37c99bdf2a6
Reamah <ezareettah@gmail.com>
2023-06-15 22:10:46 +0300

    Create array_sort.py

---
+++ array_sort.py
@@ -0,0 +1,14 @@
+def bubble_sort(arr):
+    n = len(arr)
+    
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    
+    return arr
+
+# Пример использования программы
+numbers = [5, 2, 9, 1, 3]
+sorted_numbers = bubble_sort(numbers)
+print("Отсортированный массив:", sorted_numbers)
